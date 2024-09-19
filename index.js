d12cf3b0d72c587504343a86b32dba45972ac015
mohammad syed hasan <syedhasanmohammad@gmail.com>
2024-09-20 01:01:31 +0600

    added normal server using Express & mongoDB database

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const app = express();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const port = 5000 || process.env.PORT;
+// middleware
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.grteoyu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+
+    // DATABASE
+    const payments = client.db("ssl").collection("payments");
+    // create payment api route
+    app.post("/create-payment", (req, res) => {
+      const payment_data = req?.body;
+      const initialPayment = {
+        // ebabe client theke amount neoa uchit na , server theke niba security er jonno . mane card er jinis sob server e rekhe okan theke total amount hisab korba
+        total_amount: payment_data?.total_amount,
+        currency: payment_data?.currency,
+        tran_id: "REF123", // use unique tran_id for each api call
+        success_url: "http://localhost:3030/success",
+        fail_url: "http://localhost:3030/fail",
+        cancel_url: "http://localhost:3030/cancel",
+        ipn_url: "http://localhost:3030/ipn",
+        shipping_method: "Courier",
+        product_name: "Computer.",
+        product_category: "Electronic",
+        product_profile: "general",
+        cus_name: "Customer Name",
+        cus_email: "customer@example.com",
+        cus_add1: "Dhaka",
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
+        cus_postcode: "1000",
+        cus_country: "Bangladesh",
+        cus_phone: "01711111111",
+        cus_fax: "01711111111",
+        ship_name: "Customer Name",
+        ship_add1: "Dhaka",
+        ship_add2: "Dhaka",
+        ship_city: "Dhaka",
+        ship_state: "Dhaka",
+        ship_postcode: 1000,
+        ship_country: "Bangladesh",
+      };
+      console.log("Payment data: ", payment_data);
+      res.send("result");
+    });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("ssl commerz Int server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
